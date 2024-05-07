b6093acadda8173084b8a6e725ad85c4b41bba56
Bastien <bastien.wermeille@gmail.com>
2024-05-07 09:59:59 +0200

    Porduction server

--- arcservice/cli.py
+++ arcservice/cli.py
@@ -1,11 +1,12 @@
 import logging
 
 from arcservice.app import app
+from waitress import serve
 
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-    app.run(host='0.0.0.0', port=5002)
+    serve(app, host="0.0.0.0", port=5002)
 
 
 if __name__ == "__main__":
