5a05d7a058dfd1ed019f15d16131caebff4be954
Oleg Kalashev <oleg.kalashev@gmail.com>
2025-04-10 11:10:56 +0300

    fixing tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 import requests_mock
+import os
+
 
 @pytest.fixture(scope="session")
 def app():
@@ -14,6 +16,25 @@ def app():
 
 
 @pytest.fixture(scope="session")
-def mock():
+def access_token():
+    yield "access_token"
+
+
+@pytest.fixture(scope="session")
+def refresh_token():
+    yield "refresh_token"
+
+
+@pytest.fixture(scope="session")
+def mock(access_token, refresh_token):
+    token_url = "https://keycloak.cta.cscs.ch/realms/master/protocol" \
+        "/openid-connect/token"
+
+    os.environ['DCACHE_REFRESH_TOKEN'] = 'token'
+    os.environ['DCACHE_CLIENT_SECRET'] = 'secret'
+    
     with requests_mock.Mocker() as mock:
+        mock.post(token_url,
+              json={"access_token": access_token,
+                    refresh_token: refresh_token})
         yield mock
--- tests/test_filestats.py
+++ tests/test_filestats.py
@@ -6,17 +6,7 @@ from arcservice.app import (stream_file_stats, filelist_metrics,
                             refresh_oidc_token)
 
 
-def test_refresh_oidc_token(mock):
-    token_url = "https://keycloak.cta.cscs.ch/realms/master/protocol" \
-        "/openid-connect/token"
-    access_token = "access_token"
-    refresh_token = "refresh_token"
-
-    os.environ['DCACHE_REFRESH_TOKEN'] = 'token'
-    os.environ['DCACHE_CLIENT_SECRET'] = 'secret'
-    mock.post(token_url,
-              json={"access_token": access_token,
-                    refresh_token: refresh_token})
+def test_refresh_oidc_token(mock, access_token, refresh_token):
     new_access_token = refresh_oidc_token()
 
     assert os.environ['DCACHE_REFRESH_TOKEN'] == refresh_token
@@ -24,31 +14,27 @@ def test_refresh_oidc_token(mock):
 
 
 def test_filestat(mock):
-    token_url = "https://keycloak.cta.cscs.ch/realms/master/protocol" \
-        "/openid-connect/token"
-    access_token = "access_token"
-    refresh_token = "refresh_token"
-
-    os.environ['DCACHE_REFRESH_TOKEN'] = 'token'
-    os.environ['DCACHE_CLIENT_SECRET'] = 'secret'
-    mock.post(token_url,
-              json={"access_token": access_token,
-                    refresh_token: refresh_token})
-
     dcache_url = 'https://dcache-dev.ctaodc.ch:2880'
     file_url = dcache_url + "/pnfs/cta.cscs.ch/filelists/latest"
     os.environ['DCACHE_URL'] = dcache_url
 
     # Create a string buffer
     buffer = io.StringIO()
-    buffer.write("isum,ipnfsid,path,isize,ictime,imtime,iatime,icrtime")
+    buffer.write("isum,ipnfsid,path,isize,ictime,imtime,iatime,icrtime\n")
+    file_size = 637640
+    n_files = 10
     for path_group in ['lst', 'cta', 'dteam', 'another']:
-        for _ in range(10):
-            buffer.write(f"413984,695984,/pnfs/cta.cscs.ch/{path_group}/"
-                         "673936,637640,2023-02-20 12:37:04.325,2023-02-20 "
+        for i in range(n_files):
+            buffer.write(f"413984,695984,/pnfs/cta.cscs.ch/{path_group}/f{i},"
+                         f"{file_size},2023-02-20 12:37:04.325,2023-02-20 "
                          "12:37:04.325,2023-02-20 12:37:04.18,2023-02-20 "
-                         "12:37:04.18")
-
-    mock.get(file_url, content=str(buffer))
+                         "12:37:04.18\n")
+            
+    contents = buffer.getvalue()
+    mock.get(file_url, content=contents.encode("utf-8"))
     lines = stream_file_stats()
-    print(filelist_metrics(lines))
+    metrics = filelist_metrics(lines)
+    assert metrics['lst']['file_count'] == n_files
+    assert metrics['lst']['data_size'] == n_files * file_size
+    assert metrics['total']['file_count'] == 4 * n_files
+    
