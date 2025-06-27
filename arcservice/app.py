49be14c94eea94b51cf19210a9200ef96f491f47
Oleg Kalashev <oleg.kalashev@gmail.com>
2025-06-27 15:55:36 +0200

    removing token debug output

--- arcservice/app.py
+++ arcservice/app.py
@@ -250,7 +250,7 @@ def refresh_oidc_token():
     else:
         logger.error(
             f"Error refreshing token: {response.status_code}\n"
-            f"{response.json()}\n{refresh_token}\t{client_secret}")
+            f"{response.json()}")
     return new_access_token
 
 
